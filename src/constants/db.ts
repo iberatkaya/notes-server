4588bfb8ac90b55ec82f4fd1e1a4efdec8532ab4
iberatkaya <beratkaya1998@gmail.com>
2021-04-27 16:36:21 +0300

    Added email password as environment variable

--- src/constants/db.ts
+++ src/constants/db.ts
@@ -1,13 +1,10 @@
 import { isProduction } from "../utils/utils";
-import { emailPassword } from "./secret";
 
 export const connectionString = isProduction()
   ? process.env.DATABASE_URL ?? ""
   : "mongodb://localhost:27017/db";
 
-export const emailPasswordString = isProduction()
-  ? process.env.EMAIL_PASSWORD ?? ""
-  : emailPassword;
+export const emailPasswordString = process.env.EMAIL_PASSWORD!;
 
 export const baseUrlString = isProduction()
   ? process.env.BASE_URL ?? ""
